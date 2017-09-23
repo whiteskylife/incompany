e7df51490df1f05d85e7223c2e5ebf9ad205221d
Administrator <981577040@qq.com>
2017-09-23 08:16:51 +0800

    sockets programming

---
+++ sockets IO/s1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+obj = socket.socket()
+
+obj.connect(('127.0.0.1', 9999))
+
+obj.close()
--- sockets IO/sockets IO.py
+++ sockets IO/sockets IO.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+import socket
+
+s = socket.socket()
+s.bind(('127.0.0.1', 9999,))
+s.listen(5)
+#接收客户端的请求
+
+while True:
+    conn, address = s.accept()  # accept 阻塞
+    print(address, conn)
