54b6dc66f94e083a9dbde9caa23925989b46bec0
Administrator <981577040@qq.com>
2017-09-23 08:46:49 +0800

    sockets programming

--- sockets IO/s1.py
+++ sockets IO/s1.py
@@ -5,7 +5,10 @@ import socket
 obj = socket.socket()
 
 obj.connect(('127.0.0.1', 9999))
-
+# recv阻塞，服务端没有返回结果，客户端recv永远等待
+ret_bytes = obj.recv(1024)   # 1024表示最多接收1024字节，超过1024字节下次接收
+ret_str = str(ret_bytes, encoding='utf-8')
+print(ret_str)
 obj.close()
 
 
--- sockets IO/sockets IO.py
+++ sockets IO/sockets IO.py
@@ -7,5 +7,6 @@ s.listen(5)
 #接收客户端的请求
 
 while True:
-    conn, address = s.accept()  # accept 阻塞
+    conn, address = s.accept()  # 连接，客户端地址信息 accept 阻塞
+    conn.sendall(bytes('欢饮whisky同学来到python世界', encoding='utf-8'))  # 在python3中不能直接传字符串（py2.7中可以），需要转换成字节
     print(address, conn)
