0343884d868d6d87a6f027def4b46154291f0cfa
Administrator <981577040@qq.com>
2017-09-23 12:25:31 +0800

    merge

--- sockets IO/server.py
+++ sockets IO/server.py
@@ -2,14 +2,22 @@
 import socket
 
 s = socket.socket()
-s.bind(('127.0.0.1', 9999,))
-s.listen(5)
+ip_port = ('127.0.0.1', 9999)
+s.bind(ip_port)
 #接收客户端的请求
-
+'''
 while True:
     conn, address = s.accept()  # 连接，客户端地址信息 accept 阻塞
     conn.sendall(bytes('欢饮whisky来到python世界', encoding='utf-8'))  # 在python3中不能直接传字符串（py2.7中可以），需要转换成字节
+    while True:
+        ret_bytes = conn.recv(1024)
+        ret_str = str(ret_bytes, encoding='utf-8')
+        if ret_str == 'q':
+            break
+        conn.sendall(bytes(ret_str + 'Good!', encoding='utf-8'))
+
     print(address, conn)
+'''
 
 
 
