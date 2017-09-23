8a82e68e7ddc308e8c6f9ecd2dd1de020b0d5f52
Administrator <981577040@qq.com>
2017-09-23 09:52:20 +0800

    sockets programming

--- sockets IO/sockets IO.py
+++ sockets IO/sockets IO.py
@@ -10,3 +10,7 @@ while True:
     conn, address = s.accept()  # 连接，客户端地址信息 accept 阻塞
     conn.sendall(bytes('欢饮whisky同学来到python世界', encoding='utf-8'))  # 在python3中不能直接传字符串（py2.7中可以），需要转换成字节
     print(address, conn)
+
+
+
+
