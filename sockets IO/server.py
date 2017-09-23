1a9e2efb12c7de5a113dd74220447ddcfd6aa2d4
Administrator <981577040@qq.com>
2017-09-23 18:50:38 +0800

    merge

--- sockets IO/server.py
+++ sockets IO/server.py
@@ -2,12 +2,12 @@
 import socket
 
 s = socket.socket()
-ip_port = ('127.0.0.1', 9999)
-s.bind(ip_port)
+s.bind(('127.0.0.1', 9999,))
+s.listen(5)
 #接收客户端的请求
-'''
+
 while True:
-    conn, address = s.accept()  # 连接，客户端地址信息 accept 阻塞
+    conn, address = s.accept()  # 连接，客户端地址信息 (accept 是阻塞的）
     conn.sendall(bytes('欢饮whisky来到python世界', encoding='utf-8'))  # 在python3中不能直接传字符串（py2.7中可以），需要转换成字节
     while True:
         ret_bytes = conn.recv(1024)
@@ -17,8 +17,8 @@ while True:
         conn.sendall(bytes(ret_str + 'Good!', encoding='utf-8'))
 
     print(address, conn)
-'''
 
+# 注意：传输中要把str字符串类型用bytes方法转换为字节类型，再进行传输, 输出时转换为字符串类型正常显示
 
 
 
