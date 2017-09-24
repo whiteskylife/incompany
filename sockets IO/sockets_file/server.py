3a131474e082829b2c14519d5964e6f99a375452
Administrator <981577040@qq.com>
2017-09-24 17:17:48 +0800

    merge

--- sockets IO/sockets_file/server.py
+++ sockets IO/sockets_file/server.py
@@ -1,3 +1,16 @@
 # -*- coding: utf-8 -*-
 
+import socket
+
+s = socket.socket()
+ip_port = ('127.0.0.1', 9999)
+
+s.bind(ip_port)
+
+while True:
+    conn, address = s.accept()   # accept阻塞
+    conn.sendall(bytes('欢迎登陆whisky的python世界', encoding='utf-8'))
+    # 先接收文件大小，然后开始传输文件
+    file_size = str()
+
 
