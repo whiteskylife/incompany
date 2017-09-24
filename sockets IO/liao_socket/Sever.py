e3b5e976d4dc0153f56477879d34fbc1862af03c
Administrator <981577040@qq.com>
2017-09-25 07:56:52 +0800

    merge

--- sockets IO/liao_socket/Sever.py
+++ sockets IO/liao_socket/Sever.py
@@ -9,9 +9,5 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('127.0.0.1', 9999))
 s.listen(5)                  # 调用listen()方法开始监听端口，传入的参数指定等待连接的最大数量
 print('Waiting for connection...')
-while True:
-    # 接受一个新连接:
-    sock, addr = s.accept()
-    # 创建新线程来处理TCP连接:
-    t = threading.Thread(target=tcplink, args=(sock, addr))
-    t.start()
+
+
