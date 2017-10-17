b739476670ce1bf519a495dbb28ac4ada50ada23
w-PC\w <981577040@qq.com>
2017-10-17 17:06:30 +0800

    change directory

---
+++ sockets IO/test.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import tornado.ioloop
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write('hello, 111111111111')
+
+application = tornado.web.Application(
+    [
+        (r'/', MainHandler),
+    ]
+)
+
+if __name__ == '__main__':
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
